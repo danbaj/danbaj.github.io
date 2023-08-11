bf07b055f40842067956d063d174c06356926e0d
danbaj <da9elgb@gmail.com>
2023-08-11 07:32:40 +0200

    added js

---
+++ script.js
@@ -0,0 +1,140 @@
+// Get the elements
+const timerDisplay = document.getElementById('timerDisplay');
+const startButton = document.getElementById('startButton');
+const audioTick = document.getElementById('audioTick');
+const audioEnd = document.getElementById('audioEnd');
+
+let timerSeconds = 0;
+let interval;
+let timerRunning;
+let resetClick = 0;
+var soundOn = true; 
+
+//Setup speech
+
+
+function textToSpeech(text){
+    if ("speechSynthesis" in window) {
+    const synthesis = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.rate = 0.7;
+    synthesis.speak(utterance);
+    
+  }
+  else {
+    alert("Web Speech API is not supported in this browser");
+  }
+}
+
+// Format the time as "mm:ss"
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secondsRemaining = seconds % 60;
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(secondsRemaining).padStart(2, '0');
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+// Update the timer display
+function updateTimerDisplay() {
+  timerDisplay.textContent = formatTime(timerSeconds);
+}
+
+
+// Countdown function
+function countdown() {
+  if(timerSeconds % 60 === 1){ // Notify for each passing minute
+    textToSpeech(String(Math.floor(timerSeconds / 60)) + "minutes");
+  }
+
+  timerSeconds -= 1;
+  updateTimerDisplay();
+  //audioTick.play();
+
+  if (timerSeconds <= 0) {
+    clearInterval(interval);
+    audioEnd.play();
+    timerRunning = 0;
+    textToSpeech("Stop");
+  }
+}
+
+// Attach the click event to the button
+startButton.addEventListener('click', function() {
+    if(resetClick){ //If this click is the one that resets the timer after a hold it should not add time or start the timer
+        resetClick = 0;
+    }
+    else{
+        timerSeconds += 61;
+        if(!timerRunning){
+            interval = setInterval(countdown, 1000);
+            timerRunning = 1;
+        }
+    }
+});
+
+// Initialize the timer display
+updateTimerDisplay();
+
+//Timer reset by holding button for 5 seconds
+
+class ClickAndHold {
+    /**
+     * 
+     * @param {EventTarget} target The HTML element to apply the event to 
+     * @param {Function} callback The function to run when the target is clicked and held 
+     */
+    constructor(target, callback){
+        this.target = target;
+        this.callback = callback;
+        this.isHeld = false;
+        this.activeHoldTimoutId = null;
+
+        ["mousedown", "touchstart"].forEach(type => {
+            this.target.addEventListener(type, this._onHoldStart.bind(this));
+        });
+
+        ["mouseup", "mouseleave", "mouseout", "touchend", "touchcancel" ].forEach(type => {
+            this.target.addEventListener(type, this._onHoldEnd.bind(this));
+        });
+    }
+
+    _onHoldStart(){
+        this.isHeld = true;
+
+        this.activeHoldTimoutId = setTimeout(() => {
+            if (this.isHeld){
+                this.callback();        
+            }
+        }, 5000);
+    }
+
+    
+    _onHoldEnd(){
+        this.isHeld = false;
+        clearTimeout(this.activeHoldTimoutId);
+    }
+}
+
+new ClickAndHold(startButton, () => {
+    timerSeconds = 1;
+     resetClick = 1;
+     //timerRunning = 0;
+   
+})
+
+//Hint menu
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
